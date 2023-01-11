085d338b532c9924a727d2c0cdc3a192a6ca34f8
0xDeadcell <matt2wise@gmail.com>
2023-01-11 01:48:17 -0600

    weeeee it's late

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, render_template, jsonify, url_for, redirect
+from flask import Flask, request, render_template, jsonify, url_for, redirect, send_from_directory
 from flask_scss import Scss
 from werkzeug.utils import secure_filename
 import subprocess
 import dotenv
 import json
 import os
+import re
 
 
 # Set the path to the directory containing this file
@@ -12,11 +13,10 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Create the Flask app
 app = Flask(__name__)
-app.debug = True
 app.config.from_pyfile('config.py')
 
 # Set the allowed extensions for files
-ALLOWED_EXTENSIONS = {'py', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 
 
@@ -32,24 +32,41 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
+    if app.debug:
+        print(f"Page not found: {request.url}")
     return render_template('404.html')
+
+
+@app.route('/app/<app_name>/delete', methods=['POST'])
+def delete_app(app_name):
+    app_folder = os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name))
+    if os.path.exists(app_folder):
+        # walk the directory and delete all files
+        for root, dirs, files in os.walk(app_folder, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(app_folder)
+        if app.debug:
+            print(f"Deleted app {app_name}:\nFolder: {app_folder}")
+    return redirect(url_for('index'))
 
 
 @app.route('/', methods=['POST'])
 def add_app():
-    app_name = request.form['app_name']
+    app_name = re.sub(r'\W+', '', request.form['app_name'])
     app_desc = request.form['app_desc']
     app_tag = request.form['app_tag']
     app_image = request.files['app_image']
-    print(app_name, app_desc, app_tag, app_image)
-    
-    # Check if the image file is valid
     if not allowed_file(app_image.filename):
+        print(f"Failed to create app {app_name}: Invalid file type, must be .png, .jpg, .jpeg or .gif")
         return redirect(url_for('index'))
-    # Save the file to the server
-    filename = secure_filename(app_image.filename)
+    
     app_folder = os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name))
-    print(app_folder)
+    if os.path.exists(app_folder):
+        print(f"Failed to create app {app_name}: App already exists")
+        return redirect(url_for('index'))
     os.makedirs(app_folder, exist_ok=True)
     app_image.save(os.path.normpath(os.path.join(app_folder, 'user_logo.png')))
 
@@ -61,11 +78,29 @@ def add_app():
             'desc': app_desc,
             'tag': app_tag,
     }))
-    return redirect(url_for('app', app_name=app_name))
+    if app.debug:
+        print(f"Created app {app_name}:\nFolder: {app_folder}\nImage: {app_image.filename}\nDescription: {app_desc}\nTag: {app_tag}")
+    
+    print(app_name)
+    return redirect(url_for('render_app', app_name=app_name))
+
+@app.route('/assets/apps/<app_name>/user_logo.png')
+def get_app_image(app_name):
+    if not os.path.exists(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name))):
+        return render_template('404.html')
+    if not os.path.exists(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_logo.png'))):
+        return render_template('404.html')
+    return send_from_directory(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name)), 'user_logo.png')
+
 
 @app.route('/app/<app_name>')
 def render_app(app_name):
-    with open(os.path.join(ROOT_PATH, f'assets/apps', app_name, 'details.json')) as f:
+    # Check that the app exists
+    if not os.path.exists(os.path.normpath(os.path.join(ROOT_PATH, f'assets/apps', app_name))):
+        return render_template('404.html')
+    if not os.path.exists(os.path.normpath(os.path.join(ROOT_PATH, f'assets/apps', app_name, 'details.json'))):
+        return render_template('404.html')
+    with open(os.path.normpath(os.path.join(ROOT_PATH, f'assets/apps', app_name, 'details.json'))) as f:
         app_data = json.load(f)
     return render_template('user_app_template.html', app_data=app_data)
 
@@ -73,24 +108,48 @@ def render_app(app_name):
 @app.route('/app/<app_name>/upload_script', methods=['POST'])
 def upload_script(app_name):
     script = request.files['script']
-    # Check if the script file is valid
-    if not allowed_file(script.filename):
-        return redirect(url_for('app', app_name=app_name))
-    # Save the file to the server
-    filename = secure_filename(script.filename)
-    script.save(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts', filename))
-    return redirect(url_for('app', app_name=app_name))
+    if not script.filename.endswith('.py'):
+        print(f"[!] Failed to upload script for {app_name}: Invalid file type, must be .py")
+        # return a popup saying that the file type is invalid
+        return redirect(url_for('render_app', app_name=app_name))
+    os.makedirs(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts')), exist_ok=True)
+    try:
+        try:
+            os.remove(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts', 'script.py')))
+        except Exception as e:
+            if app.debug:
+                print(f"Failed to remove old script for {app_name}: {e}")
+        script.save(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts', 'script.py')))
+    except Exception as e:
+        if app.debug:
+            print(f"Failed to upload script for {app_name}: {e}")
+    if app.debug:
+        print(f"Successfuly uploaded script for {app_name}: {script.filename}")
+    return redirect(url_for('render_app', app_name=app_name))
 
 
-@app.route('/app/<app_name>/run_script')
+@app.route('/app/<app_name>/run_script', methods=['POST'])
 def run_script(app_name):
-    script_path = os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts', 'script.py')
+    print(f"Running script for {app_name}...")
+    script_path = os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts', 'script.py'))
     if os.path.exists(script_path):
-        os.system(f'python {script_path}')
+        # Popen the script with stdout and stderr redirected
+        out, err = subprocess.Popen(['python', script_path], cwd=os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts'), stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        # Save the output to a file
+        with open(os.path.normpath(os.path.join(ROOT_PATH, 'assets/apps', app_name, 'user_scripts', 'script_log.txt')), 'w') as f:
+            f.write("STDOUT:\n")
+            f.write(out.decode('utf-8'))
+            f.write("\nSTDERR:\n")
+            f.write(err.decode('utf-8'))
+        # return jsonify({'out': out.decode('utf-8'), 'err': err.decode('utf-8')}) to the user
+        
     else:
         # Handle the error if the script doesn't exist
-        pass
-    return redirect(url_for('app', app_name=app_name))
+        print(f"{app_name} doesn't have a script")
+    if app.debug:
+        print(f"Ran script for {app_name} at {script_path}:\nSTDOUT:\n{out.decode('utf-8')}\nSTDERR:\n{err.decode('utf-8')}")
+
+    return redirect(url_for('render_app', app_name=app_name))
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -14,3 +14,5 @@ ALLOWED_EXTENSIONS = {'py'}
 PORT = os.environ.get('PORT', 80)
 DEBUG = os.environ.get('DEBUG', True)
 TESTING = os.environ.get('TESTING', True)
+TEMPLATES_AUTO_RELOAD = os.environ.get('TEMPLATES_AUTO_RELOAD', True)
+SEND_FILE_MAX_AGE_DEFAULT = os.environ.get('SEND_FILE_MAX_AGE_DEFAULT', 0)
